b75d5ab441af135275c9ae74e3f95854c210ec6b
Starley Flynn <starley@Starleys-MacBook-Air.local>
2026-06-22 19:40:06 -0700

    Updates

--- assets/ScriptPage-JaSKC3RN.js
+++ assets/ScriptPage-JaSKC3RN.js
@@ -1,5 +1,5 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ScriptResumeModal-BLOKgHvJ.js","assets/index-nYnv-I3H.js","assets/lucide-DoOgLYQ3.js","assets/index-B3gREkRa.css"])))=>i.map(i=>d[i]);
-import{j as e,w as y,a as b,_ as j}from"./index-nYnv-I3H.js";import{a,C as n,b as o,F as N,D as v}from"./lucide-DoOgLYQ3.js";const w=a.lazy(()=>j(()=>import("./ScriptResumeModal-BLOKgHvJ.js"),__vite__mapDeps([0,1,2,3])).then(i=>({default:i.ScriptResumeModal}))),m=`function findSuggestedSeatIds(count) {
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ScriptResumeModal-BA-QvtLU.js","assets/index-CouevEC4.js","assets/lucide-DoOgLYQ3.js","assets/index-B3gREkRa.css"])))=>i.map(i=>d[i]);
+import{j as e,w as y,a as b,_ as j}from"./index-CouevEC4.js";import{a,C as n,b as o,F as N,D as v}from"./lucide-DoOgLYQ3.js";const w=a.lazy(()=>j(()=>import("./ScriptResumeModal-BA-QvtLU.js"),__vite__mapDeps([0,1,2,3])).then(i=>({default:i.ScriptResumeModal}))),m=`function findSuggestedSeatIds(count) {
   const centerSeat = (seatsPerRow + 1) / 2;
   const targetRowIndex = Math.round((rows.length - 1) * 0.35);
   let bestNonAisle = null;
